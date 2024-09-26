6b25c5bb9cdafa7ff1ff388a7b4330e94eac7bd4
Collins Hillary <collinshillary1@gmail.com>
2024-09-26 22:48:26 +0300

    feat:add safari support

--- vuza_frontend/src/layout/MainLayout/index.jsx
+++ vuza_frontend/src/layout/MainLayout/index.jsx
@@ -90,7 +90,7 @@ const MainLayout = () => {
       {/* main content */}
       <Main theme={theme} open={leftDrawerOpened} sx={{backgroundColor:'#F1FFFC'}}>
         {/* breadcrumb */}
-        <Breadcrumbs separator={IconChevronRight} navigation={navigation} icon title rightAlign />
+        <Breadcrumbs separator={IconChevronRight} icon title rightAlign />
         <Outlet />
       </Main>
       {/* <Customization /> */}
